00690094a0028c704467c9b3ee546bd95f86591d
Prabuddha Gope <61056956+PrabuddhaGope@users.noreply.github.com>
2022-02-08 21:44:36 +0530

    initial commit

---
+++ app/src/main/java/com/example/dairy_mantra/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.dairy_mantra
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
