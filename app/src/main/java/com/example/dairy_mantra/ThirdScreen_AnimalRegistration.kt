8d8aaebce5fb6781a231eb80289c2008a735a064
Shreyas <shreyashk2912@gmail.com>
2022-02-11 20:00:30 +0530

    Fourth activity design

--- app/src/main/java/com/example/dairy_mantra/ThirdScreen_AnimalRegistration.kt
+++ app/src/main/java/com/example/dairy_mantra/ThirdScreen_AnimalRegistration.kt
@@ -8,6 +8,5 @@ class ThirdScreen_AnimalRegistration : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_third_screen_animal_registration)
 
-
     }
 }
