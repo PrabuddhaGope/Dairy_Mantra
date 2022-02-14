e23f66b84827de8abbdc1291cefb0489d7dee955
Shreyas <shreyashk2912@gmail.com>
2022-02-14 16:05:06 +0530

    Unresolved reference error solved

--- app/src/main/java/com/example/dairy_mantra/MainActivity.kt
+++ app/src/main/java/com/example/dairy_mantra/MainActivity.kt
@@ -1,13 +1,20 @@
 package com.example.dairy_mantra
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val button : Button = findViewById(R.id.ButtonTotalAnimal)
+        button.setOnClickListener{
+            val intent = Intent(this, ThirdScreen_AnimalRegistration::class.java)
+            startActivity(intent)
+        }
     }
 }
 
